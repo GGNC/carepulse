ce2229bbdddcbc6dfa12efbcd29e8adec8374606
Gökhan Genç <64269660+GGNC@users.noreply.github.com>
2025-01-07 12:03:57 +0300

    feat: initialize Next.js project with default settings

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "CarePulse",
+  description: "A healthcare managment system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-white">
+      Home Page
+      <Button>Click me</Button>
+    </div>
+  );
+}
